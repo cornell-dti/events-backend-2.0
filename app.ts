7deb4b4889d89adf6ceb5f2492e24fb302379042
Jagger Brulato <jb2375@cornell.edu>
2019-11-23 14:35:04 -0500

    added typing

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
+import { Request, Response } from "express-serve-static-core";
+import { Express } from "express";
+
 const express = require('express')
-const app = express()
+const app: Express = express()
 var admin = require('firebase-admin');
 const port = 3000;
 
-app.get('/', (req: any, res: any) => res.send('Hello World!'))
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
