572b86c704eb6320e57930cf3e69c802e2d5b90a
Jagger Brulato <jb2375@cornell.edu>
2019-11-23 14:29:25 -0500

    Added start script and converted to typescript

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var admin = require('firebase-admin');
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req: any, res: any) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
