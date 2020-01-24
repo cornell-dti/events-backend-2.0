4cc0a3944555ecd2c8e0790ef61bfbca8653c835
Jagger Brulato <jb2375@cornell.edu>
2020-01-23 23:41:07 -0500

    added .env support for testing

--- app/test/runTests.ts
+++ app/test/runTests.ts
@@ -6,6 +6,19 @@ var fs = require('fs');
 // Firebase --------------------------------------------------------------------
 let admin = require('firebase-admin');
 let serviceAccount = require('../../secrets/eventsbackenddatabase-firebase-adminsdk-ukak2-d1b3a5ef55.json');
+function updateServiceAccountWithSecrets() {
+  serviceAccount["private_key_id"] = process.env.PK_ID ? process.env.PK_ID : "null";
+  if (serviceAccount["private_key_id"] == "null" || !serviceAccount["private_key_id"]) {
+    console.log("ERROR: No PK_ID in .env, pk val is: " + serviceAccount["private_key_id"]);
+    throw new Error("No PK_ID in .env");
+  }
+  serviceAccount["private_key"] = process.env.PK_PWD ? process.env.PK_PWD : "null";
+  if (serviceAccount["private_key"] == "null" || !serviceAccount["private_key"]) {
+    console.log("ERROR: No PK_PWD in .env, pk_pwd val is: " + serviceAccount["private_key"]);
+    throw new Error("No PK_PWD in .env");
+  }
+}
+updateServiceAccountWithSecrets();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
