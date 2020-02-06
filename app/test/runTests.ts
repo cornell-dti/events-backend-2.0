7fff3f7313e99feaa2c0ed8b0c7f12d20ed3dcf5
Jagger Brulato <jb2375@cornell.edu>
2020-02-05 21:17:26 -0500

    Materialize now materializes using PROMISES. All endpoints are now 100% async using promise chains. All endpoints should keep this promise structure to remain non-blocking.

--- app/test/runTests.ts
+++ app/test/runTests.ts
@@ -89,13 +89,20 @@ async function runTest() {
   restrainedLog("│ " + chalk.cyan("Module") + ": " + chalk.yellow(testClassNames[classIndex]));
   restrainedLog("│ " + chalk.cyan("Running test") + ": " + chalk.magenta(currentlyRunningTest.name));
   restrainedLog("│");
-  let promise = currentlyRunningTest.apply(null, [db]);
-  promise.then(async () => {
-    nextTest();
-    if (!testsOver) {
-      await runTest();
-    }
-  });
+  await currentlyRunningTest.apply(null, [db]);
+  nextTest();
+  if (!testsOver) {
+    await runTest();
+  }
+  // let promise = currentlyRunningTest.apply(null, [db]);
+  // return promise.then(async () => {
+  //   nextTest();
+  //   if (!testsOver) {
+  //     return runTest();
+  //   } else {
+  //     return;
+  //   }
+  // });
 }
 
 function nextTest() {
--- app/util/commonOps.ts
+++ app/util/commonOps.ts
@@ -70,6 +70,90 @@ export async function materialize(object?: firestore.DocumentData, depth = maxDe
   return objectStruct;
 }
 
+export async function docRefArrayFromCollectionRefProm(coll: firestore.CollectionReference) {
+  // Init array
+  let insArr: firestore.DocumentReference[] = [];
+  // Go through each doc and add their reference
+  return coll.get().then(snapshot => {
+    snapshot.forEach(element => {
+      insArr.push(element.ref);
+    });
+    return insArr;
+  });
+}
+
+/**
+ * This function takes in a JS struct, and recursively flattens out all the
+ * references into their actual data values.
+ *
+ * Essentially, it converts all the references to their values so that the data
+ * can be passed in json.
+ *
+ * @param object The object to materialize
+ * @param depth The maximum depth of materialization (maxDepthDefault = 5)
+ */
+export async function materializeProm(object?: firestore.DocumentData, depth = maxDepthDefault) : Promise<any> {
+  return new Promise((res, rej) => {
+    if (depth <= 0) {
+       res(object);
+    }
+
+    let objectStruct = Object.assign({}, object);
+
+    let propToProm: [string, Promise<any>][] = [];
+
+    let foundAProp = false;
+
+    for (let prop in objectStruct) {
+      if (Object.prototype.hasOwnProperty.call(objectStruct, prop)) {
+        if (isDocRef(objectStruct[prop])) {
+          let ref = objectStruct[prop] as firestore.DocumentReference;
+          let dataProm = ref.get().then(val => val.data()).then((data) => {
+            return materializeProm(data, depth - 1);
+          });
+          propToProm.push([prop, dataProm]);
+          foundAProp = true;
+        }
+        else if (objectStruct[prop] instanceof Array && objectStruct[prop].length > 0
+            && isDocRef(objectStruct[prop][0])) {
+          let refArr = objectStruct[prop] as Array<firestore.DocumentReference>;
+          let groupProm = [];
+          for (let i = 0; i < refArr.length; i++) {
+            let ref = refArr[i];
+            let dataProm = ref.get().then(valRet => valRet.data()).then(data => {
+              return materializeProm(data, depth - 1);
+            });
+            groupProm.push(dataProm);
+          }
+          propToProm.push([prop, Promise.all(groupProm)]);
+          foundAProp = true;
+        }
+        else if (isCollRef(objectStruct[prop])) {
+          let collection = objectStruct[prop] as firestore.CollectionReference;
+          let promRet = docRefArrayFromCollectionRefProm(collection).then(colAsArr => {
+            return materializeProm(colAsArr, depth - 1);
+          });
+          propToProm.push([prop, promRet]);
+          foundAProp = true;
+        }
+      }
+    }
+
+    if(!foundAProp){
+      res(objectStruct);
+    }
+
+    let waitForThese : Promise<any>[] = propToProm.map(([k, v]) => v);
+
+    return Promise.all(waitForThese).then((values) => {
+      for(let i = 0; i < values.length; i++){
+        objectStruct[propToProm[i][0]] = materializeProm(values[i], depth - 1);
+      }
+      return objectStruct;
+    });
+  });
+}
+
 function isDocRef(val: any) {
   return typeof (val.collection) === 'function'
     && typeof (val.doc) === 'undefined'
