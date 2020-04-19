b431111d9d55b6aa58e163785e736286a3b96cf2
Jagger Brulato <jb2375@cornell.edu>
2020-04-18 23:58:02 -0400

    cla reset commit

--- app/app.ts
+++ app/app.ts
@@ -11,7 +11,7 @@ import * as Logger from "./logging/logger";
 // import * as handler from "./handler";
 import * as userHandler from "./handlers/userHandler";
 import uuid from "uuid";
-//custom test gary
+
 // Express ---------------------------------------------------------------------
 const express = require('express');
 const app: Express = express()
